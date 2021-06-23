15c1cef8c44f69954feed64e578101cd408cb9ca
shumynvolodymyr <shumivova@gmail.com>
2021-06-23 20:23:11 +0300

    NodeJS_DZ_1

--- app.js
+++ app.js
@@ -11,18 +11,22 @@ const fs = require('fs');
 const path = require('path');
 
 console.log("Going to create directory /dir/1800");
+
 fs.mkdir(path.join(__dirname, 'dir', '1800'), {recursive: true}, (err) => {
     if (err) {
         return console.log(err);
     }
+
     console.log("Directory created successfully!");
 });
 
 console.log("Going to create directory /dir/2000");
+
 fs.mkdir(path.join(__dirname, 'dir', '2000'), {recursive: true}, (err) => {
     if (err) {
         return console.log(err);
     }
+
     console.log("Directory created successfully!");
 });
 
@@ -32,10 +36,11 @@ const filePathOleg = path.join(__dirname, 'dir', '2000', 'Oleg.txt');
 console.log("Going to create file /dir/1800/Karina.txt");
 
 function createKarina() {
-    fs.writeFile(filePathKarina, '{"gender": "female"}', (err) => {
+    fs.writeFile(filePathKarina, ' {"gender": "female"} ', (err) => {
         if (err) {
             console.log(err);
         }
+
         console.log("File Karina.txt created successfully!");
     });
 }
@@ -48,6 +53,7 @@ function createOleg() {
         if (err) {
             console.log(err);
         }
+
         console.log("File Oleg.txt created successfully!");
     });
 }
@@ -56,89 +62,96 @@ setTimeout(createOleg, 50);
 
 ///////////////////////////////////// 1) Студентів з 1800 перевести в групу на 2000. І навпаки///////////////////////////
 
-// const dir1800 = path.join(__dirname, 'dir', '1800');
-// const dir2000 = path.join(__dirname, 'dir', '2000');
+const dir1800 = path.join(__dirname, 'dir', '1800');
+const dir2000 = path.join(__dirname, 'dir', '2000');
 
-// function transfer(oldGroup, newGroup) {
-//     fs.readdir(oldGroup, (err, files) => {
-//         if (err) {
-//             console.log(err);
-//             return
-//         }
-//         files.forEach(file => {
-//             fs.rename(path.join(oldGroup, file), path.join(newGroup, file), err => {
-//                 if (err) {
-//                     console.log(err);
-//                 }
-//                 fs.rmdir(oldGroup, {recursive: true}, (err) => {
-//                     if (err) {
-//                         console.log(err);
-//                     }
-//                 });
-//             });
-//         });
-//     });
-// }
-// setTimeout(transfer,75);
-// transfer(dir1800, dir2000);
+function transfer(oldGroup, newGroup) {
+    fs.readdir(oldGroup, (err, files) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        files.forEach(file => {
+            fs.rename(path.join(oldGroup, file), path.join(newGroup, file), err => {
+                if (err) {
+                    console.log(err);
+                }
+                fs.rmdir(oldGroup, {recursive: true}, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            });
+        });
+    });
+}
+
+setTimeout(transfer, 75);
+
+transfer(dir2000, dir1800);
 
 ///////////////////////////// 2) Перемістити всіх дівчат в папку girls а хлопців в папаку boys.//////////////////////////
 
-// const dir1800 = path.join(__dirname, 'dir', '1800');
-// const dir2000 = path.join(__dirname, 'dir', '2000');
+const dir1800 = path.join(__dirname, 'dir', '1800');
+const dir2000 = path.join(__dirname, 'dir', '2000');
 
-// function sort(directory) {
-//     fs.readdir(directory, (err,files) => {
-//         if (err) {
-//             console.log(err);
-//             return
-//         }
-//         files.forEach(file=>{
-//             fs.readFile(path.join(directory, file),(err, data)=>{
-//                 if (err) {
-//                     console.log(err);
-//                     return
-//                 }
-//
-//                 const value = JSON.parse(data.toString());
-//
-//                 if (value.gender === "female") {
-//                     fs.mkdir(path.join(__dirname, 'girls'), {recursive: true}, (err) => {
-//                         if (err) {
-//                             return console.log(err);
-//                         }
-//                         fs.rename(path.join(directory, file),path.join(__dirname, "girls", file), err1 => {
-//                             if (err1) {
-//                                 console.log(err1);
-//                             }
-//                             fs.rmdir(directory,{recursive: true}, (err)=>{
-//                                 if (err) {
-//                                     console.log(err);
-//                                 }
-//                             });
-//                         });
-//                     });
-//                 }
-//                 if (value.gender === "male") {
-//                     fs.mkdir(path.join(__dirname, 'boys'), {recursive: true}, (err) => {
-//                         if (err) {
-//                             return console.log(err);
-//                         }
-//                         fs.rename(path.join(directory, file),path.join(__dirname, "boys", file), err1 => {
-//                             if (err1) {
-//                                 console.log(err1);
-//                             }
-//                             fs.rmdir(directory,{recursive: true}, (err)=>{
-//                                 if (err) {
-//                                     console.log(err);
-//                                 }
-//                             });
-//                         });
-//                     });
-//                 }
-//             });
-//         });
-//     });
-// }
-// setTimeout(sort,100);
-// sort(dir2000);
+function sort(directory) {
+    fs.readdir(directory, (err, files) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        files.forEach(file => {
+            fs.readFile(path.join(directory, file), (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return
+                }
+
+                const value = JSON.parse(data.toString());
+
+                if (value.gender === "female") {
+                    fs.mkdir(path.join(__dirname, 'girls'), {recursive: true}, (err) => {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        fs.rename(path.join(directory, file), path.join(__dirname, "girls", file), err1 => {
+                            if (err1) {
+                                console.log(err1);
+                            }
+                            fs.rmdir(directory, {recursive: true}, (err) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                        });
+                    });
+                }
+
+                if (value.gender === "male") {
+                    fs.mkdir(path.join(__dirname, 'boys'), {recursive: true}, (err) => {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        fs.rename(path.join(directory, file), path.join(__dirname, "boys", file), err1 => {
+                            if (err1) {
+                                console.log(err1);
+                            }
+                            fs.rmdir(directory, {recursive: true}, (err) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                        });
+                    });
+                }
+            });
+        });
+    });
+}
+
+setTimeout(sort, 100);
+
+sort(dir1800);
